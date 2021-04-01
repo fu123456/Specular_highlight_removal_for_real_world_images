5da179e63c9a8cb8759c143f6f1260fcaeb2ab64
fg123456 <xyzgfu@gmail.com>
2021-04-01 21:28:09 +0800

    code for specular highlight removal

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Specular_highlight_removal_for_real_world_images
-Specular highlight removal for real-world images
+
+Note: if you have any questions, please contact me by Email: xyzgfu@gmail.com
+
+## Introduction
+Removing specular highlight in an image is a fundamental research problem in computer vision and computer graphics. While various methods have been proposed, they typically do not work well for real-world images due to the presence of rich textures, complex materials, hard shadows, occlusions and color illumination, etc. In this paper, we present a novel specular highlight removal method for real-world images. Our approach is based on two observations of the real-world images: (i) the specular highlight is often small in size and sparse in distribution; (ii) the remaining diffuse image can be represented by linear com- bination of a small number of basis colors with the sparse encoding coefficients. Based on the two observations, we design an optimization framework for simultaneously estimating the diffuse and specular highlight images from a single image. Specif- ically, we recover the diffuse components of those regions with specular highlight by encouraging the encoding coefficients sparseness using L0 norm. Moreover, the encoding coefficients and specular highlight are also subject to the non-negativity according to the additive color mixing theory and the illumination definition, respectively. Extensive experiments have been performed on a variety of images to validate the effectiveness of the proposed method and its superiority over the previous methods.
+
+This is the implementation of our paper:
+```
+@article{fu-2019-specul-highl,
+  author = {Fu, Gang and Zhang, Qing and Song, Chengfang and Lin, Qifeng and Xiao, Chunxia},
+  title = {Specular Highlight Removal for Real-World Images},
+  journal = {Computer Graphics Forum},
+  year = {2019},
+  volume = {38},
+  number = {7},
+  pages = {253--263},
+}
+```
---
+++ demo.m
@@ -0,0 +1,44 @@
+clear all;
+close all;
+clc;
+
+%% Read image, please your input name
+fullFileName='./data/fish.png';
+[~,baseFileName,~]=fileparts(fullFileName);
+img = im2double(imread(fullFileName));
+
+% Resize a large image to a small image for fast speed. Please uncomment the following code to use
+% img=imresize(img,0.5);
+
+%% Parameter setting
+% Note: there are two key parameters lambda_d and lambda_s. If you
+% want to produce better results, you can adjust them in lambda_s with
+% [0.01,0.1] and lambda_d with [1,10]. Different input highlight
+% images often have different optimal parameters. In general, lower
+% the value of lambda_s and higher the value of lambda_d, the
+% highlight layer are often with more textures, leading to the
+% texture-copy problem, while the diffuse layer is with color
+% distortion; the diffuse layer are with residual highlights, while
+% the highlight layer is more smoothness and sparse, otherwise. So
+% our algorithm is inevitably sensitive to the two key parameters.
+
+lambda_d=10; % weight for diffuse layer
+lambda_s=0.1; % weight for highlight layer
+K=100; % number of clusters
+gamma=[1/3;1/3;1/3]; % illumination color value. It's white illumination color (e.g., [1/3;1/3;1/3]) in default. You can also use existing illumination white-balance methods to estimate the illumination color of the input image
+ITERMAX=50; % number of interation for optimization
+
+% Computing diffuse and highlight layers
+[diffuse,highlight]=specular_highlight_removal(img);
+
+%% Show results
+figure(1);
+subplot(1,3,1);imshow(img);title('input image');
+subplot(1,3,2);imshow(diffuse,[]);title('diffuse image');
+subplot(1,3,3);imshow(highlight,[]);title('highlight image');
+
+% %% Save results
+% outputDir='./output/';
+% imwrite(input,[outputDir sprintf('%s.png',baseFileName)]);
+% imwrite(X_d,[outputDir sprintf('%s-diffuse.png',baseFileName)]);
+% imwrite(X_s,[outputDir sprintf('%s-highlight.png',baseFileName)]);
